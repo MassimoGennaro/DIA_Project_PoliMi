7c6b727f46346a4a9ea698b2f046d51021d10099
Massimo Gennaro <massimo.gennaro@mail.polimi.it>
2020-07-20 16:47:58 +0200

    fix indentation

--- Pricing/modules/Person_Manager.py
+++ Pricing/modules/Person_Manager.py
@@ -1,13 +1,13 @@
 import random
 
 class Person_Manager():
-	'''
-	contains the informations about the feature space and about the categories of the users
-	'''
+    '''
+    contains the informations about the feature space and about the categories of the users
+    '''
     def __init__(self, categories, probabilities,features):
-    	'''
-		receives in input the users categories, candidate probabilities and the user's features
-    	'''
+        '''
+        receives in input the users categories, candidate probabilities and the user's features
+        '''
         self.categories = categories
         self.n_categories = len(self.categories)
         self.probabilities = probabilities
@@ -18,9 +18,9 @@ class Person_Manager():
         
 
     def new_person(self):
-    	'''
-		generates a new user with a given category with uniform probability
-    	'''
+        '''
+        generates a new user with a given category with uniform probability
+        '''
         p_category = random.randint(0, self.n_categories-1) # [0,1,2]
         self.persons_count += 1
         self.categories_count[p_category] += 1
